9b4949cb945d60656d4ab8f5abfe6c4ea64b1a10
Guilherme Barreto <guibarreto64042@gmail.com>
2023-03-10 03:59:25 -0300

    Criação do arquivi acoes.js e acoes.ejs e modificações de estilo no card

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-// Nescessário para fazer os arvos js e css funcionarem, referencia onde encontrar arquivos estaticos
+// Referencia onde encontrar arquivos estaticos, como css e html
 app.use(express.static('C:/Sapien'))
 
 // Para retornar no render(/) precisa ser arquivo ejs, e para achar ele esse código o referencia
@@ -10,6 +10,10 @@ app.set('view engine', 'ejs');
 app.get("/", (req, res) => {
   res.render('index')
 })
+
+app.get("/acoes", (req, res) => {
+  res.render('acoes')
+} )
 
 app.listen(9090, () => {
   console.log("Sapien tá ON!!")
